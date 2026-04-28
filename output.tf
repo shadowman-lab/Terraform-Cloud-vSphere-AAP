3dfd0dfa03b33ad881c23f2352b99fa8e2f87b50
Alex Dworjan <adworjan@redhat.com>
2026-04-28 12:03:05 -0400

    add url and status to output

--- output.tf
+++ output.tf
@@ -4,3 +4,11 @@
 output "vm_name_terraformvms" {
   value = vsphere_virtual_machine.terraformvms[*].name
 }
+
+output "aap_job_url" {
+  value = aap_workflow_job.configure_vms.url
+}
+
+output "aap_job_url" {
+  value = aap_workflow_job.configure_vms.status
+}
